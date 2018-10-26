dc8dbf5d058793617f5e1b2710dc55e1837c8655
JPHaus <jonathan.porter47@gmail.com>
2018-10-26 10:13:51 -0400

    Removed folders from repository

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/home/jon/Source/civichackathons.com/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/home/jon/Source/civichackathons.com/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
