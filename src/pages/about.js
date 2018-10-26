f451d1c78c3204b486cf5fafdf9c97b5cb88a85e
JPHaus <jonathan.porter47@gmail.com>
2018-10-25 22:24:25 -0400

    Added logo

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Layout from '../components/layout';
 
-import Layout from '../components/layout'
+import logo from '../images/logo-500x500.png';
 
 const AboutPage = () => (
     <Layout>
       <h1>About</h1>
-      <div align="center"><img src="https://avatars0.githubusercontent.com/u/43707411?s=400&u=7bb848465cca81363501569aac30172518282174&v=4" alt="Civic Hackathons Logo" /></div>
+      <div align="center"><img src={logo} alt="Civic Hackathons Logo" /></div>
       <p>Dedicated to helping civic organizations engage with their community and tackle challenging civic issues.</p>
     </Layout>
   )
