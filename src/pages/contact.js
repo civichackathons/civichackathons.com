fb515c0b14f56e52197c39c7aab317a0f38fc451
JPHaus <jonathan.porter47@gmail.com>
2018-10-28 20:42:15 -0400

    Updated contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const ContactPage = () => (
+  <Layout>
+    <h1>Let's get in touch</h1>
+    <p>Send us a message at <a href="mailto:info@civichackathons.com">info@civichackathons.com</a></p>
+  </Layout>
+)
+
+export default ContactPage
