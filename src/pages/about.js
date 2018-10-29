d6f620f386605fad953ddcedd11f55c95417e2a6
JPHaus <jonathan.porter47@gmail.com>
2018-10-28 20:41:14 -0400

    Updated about page with contact info and description

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { Link } from 'gatsby';
 
 import logo from '../images/logo-500x500.png';
 
@@ -7,8 +8,13 @@ const AboutPage = () => (
     <Layout>
       <h1>About</h1>
       <div align="center"><img src={logo} alt="Civic Hackathons Logo" /></div>
-      <p>Dedicated to helping civic organizations engage with their community and tackle challenging civic issues.</p>
+      <p>
+        Civic Hackathons was created to help give you the resources and information needed to run a successful civic-oriented hackathon. 
+        These hackathons are a great opportunity to engage with your community and tackle challenging civic issues together.
+        The information and resources on this site are compiled from other organizations that have successfully run civic hackathons.
+      </p>
+      <p>Want to help? Check out our <a href="https://github.com/civichackathons">github</a> or <Link to="/contact">send us a message</Link>!</p>
     </Layout>
   )
   
-  export default AboutPage
+export default AboutPage
